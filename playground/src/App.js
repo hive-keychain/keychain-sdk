ed980e8e09e74c5bd104b1847afb5c431b13cbb7
theghost1980 <saturnob612@gmail.com>
2023-02-14 10:52:04 -0400

    fix: clean up code

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,22 +1,88 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
-const logo_svg_1 = __importDefault(require("./logo.svg"));
-require("./App.css");
+/*global chrome*/
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+import { KeychainSDK } from 'keychain-sdk';
+
 function App() {
-    return (<div className="App">
+  //using DEFAULT RPC.
+  // const KeychainSDK = new KeychainSDK(window, { rpc: 'DEFAULT' });
+
+  // no RPC
+  const sdk = new KeychainSDK(window);
+
+  // tests on requestHandshake
+  // useEffect(() => {
+  //   const onLoadHandler = async () => {
+  //     console.log('Fully loaded!');
+  //     if (window.hive_keychain) {
+  //       console.log('It should has it!!');
+  //       const cbPromise = function () {
+  //         return new Promise((resolve, rejects) => {
+  //           console.log('Waited and resolved!');
+  //           resolve(true);
+  //         });
+  //       };
+  //       window.hive_keychain.requestHandshake(
+  //         cbPromise().then((value) => {
+  //           console.log({ value });
+  //         }),
+  //       );
+  //     }
+  //   };
+  //   window.addEventListener('load', onLoadHandler);
+  // });
+
+  //end tests
+
+  //Stays as useful hook that may be
+  //used as SDK on Hooks for: ReactJS, ANG, etc
+
+  useEffect(() => {
+    const onLoadHandler = async () => {
+      console.log('Fully loaded!');
+      try {
+        const login = await sdk.login(
+          {
+            username: undefined,
+            message: 'message!!',
+            method: 'Active',
+            title: 'Login in Into Saturnoman.com\nProceed?',
+          },
+          {},
+        );
+        console.log({ login });
+      } catch (error) {
+        console.log({ error });
+      }
+    };
+
+    window.addEventListener('load', onLoadHandler);
+
+    return () => {
+      window.removeEventListener('load', onLoadHandler);
+    };
+  });
+
+  //end useful hook.
+
+  return (
+    <div className="App">
       <header className="App-header">
-        <img src={logo_svg_1.default} className="App-logo" alt="logo"/>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
-    </div>);
+    </div>
+  );
 }
-exports.default = App;
+
+export default App;
